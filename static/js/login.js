4c436d71f86eafce9e5ac59cc0d31b150dc2f11d
marcjannabubak <marcjanna.bubak@gmail.com>
2026-05-24 18:06:13 +0200

    page design and login working

---
+++ static/js/login.js
@@ -0,0 +1,7 @@
+document.getElementById('openLogin').addEventListener('click', function() {
+    document.getElementById('loginOverlay').style.display = 'flex';
+});
+
+document.getElementById('closeLogin').addEventListener('click', function() {
+    document.getElementById('loginOverlay').style.display = 'none';
+});
